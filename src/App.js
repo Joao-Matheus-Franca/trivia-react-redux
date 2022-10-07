c1fc9d770ebebe3b63ff5a99da3cd18592053e05
Juan Fernando De Moura Silva <juanfernandodemourasilva@gmail.com>
2022-10-07 17:09:26 -0300

    reuisito-03

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Settings from './pages/Settings';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route exact path="/Configurações" component={ Settings } />
     </Switch>
   );
 }
