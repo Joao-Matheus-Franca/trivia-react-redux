86d421fc85798165b856b13d88c53c26a67774e3
Fabio Penna <ffmpenna@gmail.com>
2022-10-10 14:05:53 -0300

    Requisito 7

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -4,11 +4,49 @@ import { connect } from 'react-redux';
 import Header from '../components/Header';
 
 class Game extends React.Component {
+  state = {
+    correctAnswers: [],
+    incorrectAnswers: [],
+  };
+
+  componentDidMount() {
+    this.teste();
+  }
+
+  teste = () => {
+    const { questions } = this.props;
+
+    const corrects = questions.map((q) => q.correct_answer);
+    const incorrects = questions.map((q) => q.incorrect_answers);
+
+    this.setState({ correctAnswers: corrects, incorrectAnswers: incorrects });
+  };
+
+  handleClick = ({ target }) => {
+    const { correctAnswers, incorrectAnswers } = this.state;
+    const btns = document.querySelectorAll('.answer-btn');
+
+    // Coloquei esse log aqui porque vamos precisar dessas informações no futuro
+    // e o lint tava reclamando que não tava sendo usado em nenhum lugar
+    // então taquei elas num console.log pra não perder as informações
+    console.log(incorrectAnswers, target);
+
+    btns.forEach((b) => {
+      if (correctAnswers.some((a) => a === b.innerText)) {
+        return b.classList.add('correta');
+      }
+      return b.classList.add('incorreta');
+    });
+  };
+
   render() {
     const { questions } = this.props;
     const number = -1;
     const maxNumber = 1;
-    const array = [questions[0].correct_answer, ...questions[0].incorrect_answers];
+    const array = [
+      questions[0].correct_answer,
+      ...questions[0].incorrect_answers,
+    ];
     array.sort(() => {
       if (Math.round(Math.random()) === maxNumber) {
         return number;
@@ -18,33 +56,35 @@ class Game extends React.Component {
     return (
       <>
         <Header />
-        <h3 data-testid="question-category">{ questions[0].category }</h3>
-        <h2 data-testid="question-text">{ questions[0].question }</h2>
+        <h3 data-testid="question-category">{questions[0].category}</h3>
+        <h2 data-testid="question-text">{questions[0].question}</h2>
         <div data-testid="answer-options" className="options">
-          {
-            array.map((e, i) => {
-              if (e === questions[0].correct_answer) {
-                return (
-                  <button
-                    key={ i }
-                    type="button"
-                    data-testid="correct-answer"
-                  >
-                    { e }
-                  </button>
-                );
-              }
+          {array.map((e, i) => {
+            if (e === questions[0].correct_answer) {
               return (
                 <button
                   key={ i }
                   type="button"
-                  data-testid={ `wrong-answer-${i}` }
+                  data-testid="correct-answer"
+                  onClick={ this.handleClick }
+                  className="answer-btn"
                 >
-                  { e }
+                  {e}
                 </button>
               );
-            })
-          }
+            }
+            return (
+              <button
+                key={ i }
+                type="button"
+                data-testid={ `wrong-answer-${i}` }
+                onClick={ this.handleClick }
+                className="answer-btn"
+              >
+                {e}
+              </button>
+            );
+          })}
         </div>
       </>
     );
