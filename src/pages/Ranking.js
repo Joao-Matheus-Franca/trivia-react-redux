cd5f6848a1390f8d6fad487c750263427a747504
João Matheus Silva Franca <matheussfranca@live.com>
2022-10-11 16:09:26 -0300

    Concluido: Requisito 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { playAgain } from '../redux/actions/fetchActions';
 
 class Ranking extends React.Component {
+  goHome = () => {
+    const { history, dispatch } = this.props;
+    dispatch(playAgain());
+    history.push('/');
+  };
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ this.goHome }
+          data-testid="btn-go-home"
+        >
+          Tela inicial
+        </button>
+      </>
     );
   }
 }
 
-export default Ranking;
+Ranking.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default connect()(Ranking);
